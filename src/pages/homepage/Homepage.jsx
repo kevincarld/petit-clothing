bff9939783d1bf6b592d4021ea1a66d2cc6fa89d
Kevin David <kevin@jaladesign.com.au>
2020-02-24 20:01:27 +1000

    Additon: Directory and menu item components and styles

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Components: Directory
+import Directory from '../../components/directory/Directory'
+
+const Homepage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+)
+
+export default Homepage;
